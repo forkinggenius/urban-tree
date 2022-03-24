555cbd148ef545f47e7b26222926d61ba585d4fc
Nizar Mahmoud <nizarmah@hotmail.com>
2022-03-24 09:12:58 +0200

    Refactor of tree to separate data structure and nodes
    This change refactors the previous tree nodes which contained the memoization because they had a specific bug
    
    The previous bug was when you're trying to delete a root node
    
    The root nodes wouldn't get deleted because they were being stored outside the data structure
    
    Accordingly, this change moves the logic in the tree nodes out to a tree data structure which handles the root nodes and the memoization
    
    The addition and removal functionalities were slightly changed in code, but not in logic
    
    It is important to mention that some logic was added regarding the root nodes and removing the root node in case it's being unlinked

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,36 @@
 import chokidar from 'chokidar';
 import express from 'express';
 
-import { FileTreeNode } from './models/fileTreeNode';
+import { FileTree } from './models/fileTree';
 
 const app = express();
 const port = 3000;
 
-const rootDirectoryNodes: FileTreeNode[] = [];
+const queuedRootDirectories = process.argv.splice(2);
+console.debug(`queued directories: ${queuedRootDirectories}`);
 
-const queuedDirectories = process.argv.splice(2);
-console.debug(`queued directories: ${queuedDirectories}`);
-
-queuedDirectories.forEach(
-    directoryPath => rootDirectoryNodes.push(FileTreeNode.get(directoryPath)));
+FileTree.initialize(queuedRootDirectories);
 
 // no need to specify every sub-directory, because library already traverses
 // in depth. so it's only necessary to put the top-level directory
 // also no need to worry about passing a directory and a sub-directory, because
 // this is a single watcher, meaning it ignores duplicates
 // use `ignoreInitial` so we don't get add events when watcher is getting ready
-const directoryChangeWatcher = chokidar.watch(queuedDirectories, {
+const directoryChangeWatcher = chokidar.watch(queuedRootDirectories, {
     followSymlinks: false,
     ignoreInitial: true,
 });
 
 directoryChangeWatcher
-    .on('add', path => FileTreeNode.add(path))
-    .on('addDir', path => FileTreeNode.add(path))
-    .on('unlink', path => FileTreeNode.unlink(path))
-    .on('unlinkDir', path => FileTreeNode.unlink(path));
+    .on('add', path => FileTree.addNode(path))
+    .on('addDir', path => FileTree.addNode(path))
+    .on('unlink', path => FileTree.unlinkNode(path))
+    .on('unlinkDir', path => FileTree.unlinkNode(path));
 
 app.get('/', (req, res) => {
     res.json(
         {
-            directories: rootDirectoryNodes,
+            directories: FileTree.roots,
         }
     );
 });
--- src/models/fileTreeNode.ts
+++ src/models/fileTreeNode.ts
@@ -4,15 +4,13 @@ import path from 'path';
 import { JsonMap as Map } from '../utils/jsonMap';
 
 export class FileTreeNode {
-    static memo = new Map<string, FileTreeNode>();
-
     filePath = "";
     baseName = "";
 
     isDirectory = false;
     childNodes = new Map<string, FileTreeNode>();
 
-    private constructor(filePath: string) {
+    constructor(filePath: string) {
         this.filePath = filePath;
         this.baseName = path.parse(filePath).base;
 
@@ -21,65 +19,6 @@ export class FileTreeNode {
         if (fs.existsSync(this.filePath) && 
                 fs.lstatSync(this.filePath).isDirectory()) {
             this.isDirectory = true;
-            this.collectChildNodes();
         }
-    }
-
-    static get(filePath: string) {
-        if (!this.memo.has(filePath)) {
-            this.memo.set(filePath, new FileTreeNode(filePath));
-        }
-
-        return this.memo.get(filePath);
-    }
-
-    private static deleteFromMemo(node: FileTreeNode) {
-        this.memo.delete(node.filePath);
-    }
-
-    static add(filePath: string) {
-        const parentFilePath = path.dirname(filePath);
-        const newlyAddedNode = this.get(filePath);
-
-        this.addNodeToParent(parentFilePath, newlyAddedNode);
-    }
-
-    private static addNodeToParent(parentFilePath: string, node: FileTreeNode) {
-        const parentNode = this.get(parentFilePath);
-
-        parentNode.childNodes.set(node.baseName, node);
-
-        console.debug(`adding node: ${node.baseName}`);
-        console.debug(JSON.stringify(parentNode, null, 4));
-    }
-
-    static unlink(filePath: string) {
-        const currentNode = this.get(filePath);
-
-        this.unlinkNodeFromParent(currentNode);
-    }
-
-    private static unlinkNodeFromParent(node: FileTreeNode) {
-        const parentFilePath = path.dirname(node.filePath);
-        const parentNode = this.get(parentFilePath);
-
-        parentNode.childNodes.delete(node.baseName);
-
-        this.deleteFromMemo(node);
-        node.childNodes.forEach(childNode => {
-            this.deleteFromMemo(childNode);
-        });
-
-        console.debug(`unlinking node: ${node.baseName}`);
-        console.debug(JSON.stringify(parentNode, null, 4));
-    }
-
-    private collectChildNodes() {
-        fs.readdirSync(this.filePath).forEach(childFileName => {
-            const childFilePath = path.join(this.filePath, childFileName);
-            const node = FileTreeNode.get(childFilePath);
-
-            this.childNodes.set(node.baseName, node);
-        });
     }
 }
