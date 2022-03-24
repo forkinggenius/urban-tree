543c1c6025fc4eb58689e9f469cf775abcf1ef83
Nizar Mahmoud <nizarmah@hotmail.com>
2022-03-24 09:31:51 +0200

    Implement server sent events for file updates
    Utilizes digital ocean's tutorial, [_How To Use Server-Sent Events in Node.js to Build a Realtime App_](https://www.digitalocean.com/community/tutorials/nodejs-server-sent-events-build-realtime-app) to sent events about file and directory changes to the file explorer

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import chokidar from 'chokidar';
+import cors from 'cors';
 import express from 'express';
 
 import { FileTree } from './models/fileTree';
 
+const SSE_HEADERS = {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache'
+};
+
 const app = express();
 const port = 3000;
+
+app.use(cors());
+
+let clients = [];
 
 const queuedRootDirectories = process.argv.splice(2);
 console.debug(`queued directories: ${queuedRootDirectories}`);
@@ -21,18 +32,58 @@ const directoryChangeWatcher = chokidar.watch(queuedRootDirectories, {
     ignoreInitial: true,
 });
 
-directoryChangeWatcher
-    .on('add', path => FileTree.addNode(path))
-    .on('addDir', path => FileTree.addNode(path))
-    .on('unlink', path => FileTree.unlinkNode(path))
-    .on('unlinkDir', path => FileTree.unlinkNode(path));
+// we watch for all events because it's the cleanest way to always post events
+// to our clients, without having to repeat any code in a really ugly manner
+directoryChangeWatcher.on('all', (event, path) => {
+    // matches ['add', 'addDir']
+    const isAddEvent = event.startsWith('add');
+    
+    // matches ['unlink', 'unlinkDir'] 
+    const isUnlinkEvent = event.startsWith('unlink');
+    
+    // if is either add or unlink, we need to publish event
+    if (isAddEvent || isUnlinkEvent) {
+        const data = {
+            action: '',
+            path: path,
+            node: '',
+        };
+
+        if (isAddEvent) {
+            FileTree.addNode(path);
+
+            data.action = 'add';
+            data.node = JSON.stringify(FileTree.getNode(path));
+        } else if (isUnlinkEvent) {
+            data.action = 'unlink';
+
+            FileTree.unlinkNode(path);
+        }
+
+        clients.forEach(client => {
+            client.res.write(`data: ${JSON.stringify(data)}\n\n`)
+        });
+    }
+});
+
+app.get('/status', (req, res) => res.json({clients: clients.length}));
 
 app.get('/', (req, res) => {
-    res.json(
-        {
-            directories: FileTree.roots,
-        }
-    );
+    res.writeHead(200, SSE_HEADERS);
+    res.write(`data: ${JSON.stringify(FileTree.roots)}\n\n`);
+
+    const clientId = Date.now();
+    
+    const newClient = {
+        id: clientId,
+        res,
+    };
+    clients.push(newClient);
+
+    req.on('close', () => {
+        console.debug(`connection closed: ${clientId}`);
+        clients = clients.filter(client => client.id !== clientId);
+    });
 });
 
 app.listen(port, () => {
